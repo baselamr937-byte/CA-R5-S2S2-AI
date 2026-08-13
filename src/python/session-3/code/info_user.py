dc77d0707979b04feb105796f176b846702f1bdf
baselamr937-byte <baselamr937@gmail.com>
2026-08-13 19:08:22 +0300

    assignment_2

---
+++ src/python/session-3/code/info_user.py
@@ -0,0 +1,13 @@
+def user_info(name: str, age: int) ->str :
+    '''  
+    brief description of your function
+    Args:
+        param_name(str): this is string input for name of user
+        param_age(int): this is int input fot the user aga 
+    Returns:
+        return_type: str
+    Examples:
+        >>> user_info("Alice", 30)
+        my name is Alice, my age is 30
+    '''
+    return f"my name is {name}, my age is {age}"
---
+++ src/python/session-3/code/my_func.py
@@ -0,0 +1,15 @@
+def func_1():
+    ''' 
+    brief description of the function
+    This function prints a greeting message to the console.
+    
+    arguments: 
+        None
+    
+    
+    return:
+        print statement
+    
+    
+    '''
+    print("Hello from func_1")
